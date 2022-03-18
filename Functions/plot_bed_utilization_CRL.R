d2b3e9295e6765712e208bab3dc43b0c9e205fb2
sina mokhtar <sinamokhtar@tamu.edu>
2022-03-18 02:56:47 -0500

    initial commit

---
+++ Functions/plot_bed_utilization_CRL.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(usdata)
+library(data.table)
+library(ggplot2)
+library(scales)
+
+load("Data/CDC_risk_level_new.csv")
+
+
+df = na.omit(CDC_risk_clean_new)
+df$risk_level = as.factor(df$risk_level)
+
+fig7 = ggplot(df, aes(x=risk_level,y=bed_utilization, color=risk_level))+
+    geom_jitter(position = position_jitter(width = 0.02))+
+    geom_boxplot(alpha = 0.7)+
+    coord_flip()+
+    geom_rug()
+    
+
+ggsave("Result/Fig7.jpg",fig7, height=4,width=8,scale=1.65)
+
