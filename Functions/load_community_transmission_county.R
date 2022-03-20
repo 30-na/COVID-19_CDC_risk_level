8037c4ff7855c96641608ff06dea4451352a304d
sina mokhtar <sinamokhtar@tamu.edu>
2022-03-19 19:00:46 -0500

    add positive test coulmn

--- Functions/load_community_transmission_county.R
+++ Functions/load_community_transmission_county.R
@@ -8,13 +8,11 @@ CDC_risk = fread("Data/United_States_COVID-19_County_Level_of_Community_Transmis
 
 #clean data
 CDC_community_transmission = CDC_risk %>%
-    select(report_date,
-           state_name,
-           county_name,
-           community_transmission_level)%>%
     rename(date = report_date,
            state = state_name,
            county = county_name,
+           new_case = cases_per_100K_7_day_count_change,
+           positive_test = percent_test_results_reported_positive_last_7_days,
            risk_level = community_transmission_level) %>%
     mutate(date = as.Date(date, format="%Y/%m/%d"),
            state = state2abbr(state),
@@ -23,7 +21,7 @@ CDC_community_transmission = CDC_risk %>%
                                         "moderate",
                                         "substantial",
                                         "high"))) %>%
-    arrange(date, state, county) %>%
+    arrange(date, state, fips_code) %>%
     filter(date >= "2021/01/01")
 
 save(CDC_community_transmission, file="Data/CDC_community_transmission_county.csv") 
