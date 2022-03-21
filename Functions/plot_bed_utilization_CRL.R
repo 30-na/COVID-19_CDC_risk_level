205a4ad602c24aac4967f2404ad22df83b689553
30-na <sinamokhtar@tamu.edu>
2022-03-21 13:42:42 -0500

    add all plots for CDC Community Level

--- Functions/plot_bed_utilization_CRL.R
+++ Functions/plot_bed_utilization_CRL.R
@@ -5,7 +5,35 @@ library(ggplot2)
 library(scales)
 library(ggpubr)
 
-load("Data/CDC_risk_level_new.csv")
+load("Data/CDC_community_level_county.csv")
+load("Data/CDC_community_level_county_computed.csv")
+
+#top 50 county base on the population
+
+top50_county = CDC_community_level_county %>%
+    arrange(desc(population)) %>% 
+    slice(1:100)
+
+
+
+top50_county_list = unique(top50_county$county_fips)
+
+consistent_CRL = CDC_community_level_county %>%
+    dplyr::filter(county_fips %in% top50_county_list)
+
+
+fig6 = ggplot(consistent_CRL, aes(x=date_updated, y=risk_level,
+                                  group=1, color=risk_level))+
+    geom_point()+
+    geom_line()+
+    facet_wrap(~county_fips, ncol=5)+
+    scale_x_date(date_breaks = "1 week",
+                 date_labels = "%b/%d",
+                 date_minor_breaks = "1 day",
+                 name= "Updated Date")+
+    labs(title="Community Risk Level Consistency in 50 most populous counties")
+
+ggsave("Result/Fig6.jpg",fig6, height=8,width=4,scale=1.65)
 
 fig7 = ggplot(na.omit(CDC_risk_clean_new), aes(x=risk_level,y=bed_utilization, color=risk_level))+
     geom_jitter(position = position_jitter(width = 0.02))+
@@ -39,9 +67,6 @@ consistent_low = CDC_risk_clean_new %>%
                risk_level == "Low" &
                county_fips %in% first_low$county_fips)
 
-nrow(no_consistent_low)
-nrow(first_low)
-nrow(consistent_low)
 
 
 first_medium = CDC_risk_clean_new %>%
@@ -56,9 +81,7 @@ consistent_medium = CDC_risk_clean_new %>%
     filter(date_updated == c("2022-03-10") &
                risk_level == "Medium" &
                county_fips %in% first_medium$county_fips)
-nrow(no_consistent_medium)
-nrow(consistent_medium)
-nrow(first_medium)
+
 
 first_high = CDC_risk_clean_new %>%
     filter(date_updated == c("2022-03-03") &
@@ -72,9 +95,7 @@ consistent_high = CDC_risk_clean_new %>%
     filter(date_updated == c("2022-03-10") &
                risk_level == "High" &
                county_fips %in% first_high$county_fips)
-nrow(consistent_high)
-nrow(first_high)
-nrow(no_consistent_high)
+
 
 consistant_table=data.frame("Risk.Level.Counties" = c("Low", "Medium", "High"),
                             
