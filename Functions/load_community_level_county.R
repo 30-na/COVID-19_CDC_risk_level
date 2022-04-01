983f0673bc16b1c8b6012fda1279d963f95f6f2b
sina mokhtar <sinamokhtar@tamu.edu>
2022-04-01 00:11:10 -0500

    add new name

--- Functions/load_community_level_county.R
+++ Functions/load_community_level_county.R
@@ -6,17 +6,20 @@ library(data.table)
 # read data from CDC Community Risk level 
 CDC_risk_new = fread("Data/United_States_COVID-19_Community_Levels_by_County.csv")
 
+
 #clean data
 CDC_community_level_county = CDC_risk_new %>%
     select(c(date_updated,
            state,
            county_fips,
            total_population,
+           covid_cases_per_100k,
            covid_hospital_admissions_per_100k,
            covid_inpatient_bed_utilization) |
            ends_with("community_level"))%>%
     rename(risk_level = ends_with("community_level"),
            population = total_population,
+           new_case = covid_cases_per_100k,
            bed_utilization = covid_inpatient_bed_utilization,
            hospital_admission = covid_hospital_admissions_per_100k)%>%
     mutate(date_updated = as.Date(date_updated, format="%Y/%m/%d"),
