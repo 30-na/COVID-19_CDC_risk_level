e4c6a980b59b35c2a912f76fdc12d00461cc1724
30-na <sinamokhtar@tamu.edu>
2022-04-03 09:42:52 -0500

    initial commit

--- Functions/plots_computed_consistency.R
+++ Functions/plots_computed_consistency.R
@@ -28,160 +28,80 @@ CL_common = community_level_county_computed %>%
 
 
 # four weeks interval 
-fourweek_interval = community_level_county_computed
-fourweek_interval$consis = 0
-a = c()
-for(i in seq(1, length(days)-1, by=4)){
-    print(days[i])
-    community_level_county_computed = community_level_county_computed %>%
-        filter(date == days[i] |
-                          date == days[i+1] | 
-                          date == days[i+2] |
-                          date == days[i+3])
+
+consis = CL_common %>%
+    select(date,
+           fips_code,
+           community_level) %>%
+    group_by(fips_code) %>%
+    arrange(fips_code,
+            date)
+consis_4weeks = c()
+for(i in 4:nrow(consis)){
+    consis_4weeks[i] = length(unique(c(consis$community_level[i],
+                 consis$community_level[i-1],
+                 consis$community_level[i-2],
+                 consis$community_level[i-3])))
+   
+    }
     
-    fourweek_interval$consis[i] = length(unique(fourweek_interval$community_level))
+consis$consis_4weeks = consis_4weeks
+
+consis = filter(consis, date > "2020-08-14")
+
+
+consis_plot = consis %>%
+    arrange(date) %>%
+    group_by(date, community_level) %>%
+    count(consis_4weeks) %>%
+    mutate(consisRate = n/length(common_counties))%>%
+    filter(consis_4weeks == 1)%>%
+    arrange(date, community_level)
+
+
+
+
+b3 = ggplot(consis_plot, aes(x=date, y=consisRate,
+                                  group=1, color=community_level))+
+    
+    geom_line()+
+    labs(title="Community Risk Level Consistency-4weeks")+
+    facet_wrap(~community_level, ncol=3)
+
+ggsave("Result/b3.jpg",b1, height=4,width=8,scale=1.65)
+
+
+
+
+#################################################
+consis_2weeks = c()
+for(i in 2:nrow(consis)){
+    consis_2weeks[i] = length(unique(c(consis$community_level[i],
+                                       consis$community_level[i-1])))
     
 }
 
+consis$consis_2weeks = consis_2weeks
 
-    g = ggplot(fourweek_interval, aes(x=date, y=community_level,
-                                      group=1, color=community_level))+
-        geom_point()+
-        geom_line()+
-        facet_wrap(~fips_code, ncol=5)+
-        scale_x_date(date_breaks = "1 week",
-                     date_labels = "%Y/%b",
-                     date_minor_breaks = "1 day",
-                     name= "Updated Date")+
-        labs(title="computed Community level in 50 counties")
-    ggsave(paste("Result/fourWeeks", i, ".jpg", sep=""),g, height=16,width=8,scale=1.65)
-    
-}
+consis = filter(consis, date > "2020-08-14")
 
-for(i in seq(1, length(days)-1, by=4)){
-    community_level_count = community_level_county_computed %>%
-        filter(fips_code %in% common_counties) %>%
-        filter(date == days[i] |
-                   date == days[i+1] | 
-                   date == days[i+2] |
-                   date == days[i+3]) %>%
-        group_by(fips_code,
-                 community_level) %>%
-        summarise(n = n()) %>%
-        arrange(fips_code)
-    consistent_low = community_level_count %>%
-        filter(community_level == "Low" &
-                   n == 4)
-    
-    consistent_medium = community_level_count %>%
-        filter(community_level == "Medium" &
-                   n == 4)
-    
-    consistent_high = community_level_count %>%
-        filter(community_level == "High" &
-                   n == 4)
-    no_consistent = community_level_count %>%
-        filter(n != 4)
-    
-    consistant_table = data.frame("Risk.Level.Counties" = c("Low", "Medium", "High", "Total"),
-                                  
-                                  "Consistant" = c(nrow(consistent_low),
-                                                   nrow(consistent_medium),
-                                                   nrow(consistent_high),
-                                                   nrow(total_consistent)),
-                                  
-                                  "Not.Consistant" = c(nrow(no_consistent)),
-                                  
-                                  "Consistency.Rate" = c(nrow(consistent_low)/nrow(community_level_count),
-                                                         nrow(consistent_medium)/nrow(community_level_count),
-                                                         nrow(consistent_high)/nrow(community_level_count),
-                                                         nrow(total_consistent)/nrow(community_level_count)))
-    
-    
-    fig = ggtexttable(consistant_table, rows = NULL, 
-                      theme = ttheme("mOrange"))
-    ggsave(paste("Result/fourWeeks.t", i, ".jpg", sep=""),fig, height=3,width=9,scale=1)
-}
+
+consis_plot = consis %>%
+    arrange(date) %>%
+    group_by(date, community_level) %>%
+    count(consis_2weeks) %>%
+    mutate(consisRate2 = n/length(common_counties))%>%
+    filter(consis_2weeks == 1)%>%
+    arrange(date, community_level)
 
 
 
 
+b2 = ggplot(consis_plot, aes(x=date, y=consisRate2,
+                             group=1, color=community_level))+
+    
+    geom_line()+
+    labs(title="Community Risk Level Consistency-2weeks")+
+    facet_wrap(~community_level, ncol=3)
 
-# two weeks interval 
-
-for(i in seq(1, length(days)-1, by=2)){
-    fourweek_interval = community_level_county_computed %>%
-        filter(date == days[i] |
-                   date == days[i+1]) %>%
-        filter(fips_code %in% top50_county$fips_code)
-    
-    
-    g = ggplot(fourweek_interval, aes(x=date, y=community_level,
-                                      group=1, color=community_level))+
-        geom_point()+
-        geom_line()+
-        facet_wrap(~fips_code, ncol=5)+
-        scale_x_date(date_breaks = "1 week",
-                     date_labels = "%Y/%b",
-                     date_minor_breaks = "1 day",
-                     name= "Updated Date")+
-        labs(title="computed Community level in 50 counties")
-    ggsave(paste("Result/twoWeeks", i, ".jpg", sep=""),g, height=16,width=8,scale=1.65)
-    
-}
-
-for(i in seq(1, length(days)-1, by=2)){
-    community_level_count = community_level_county_computed %>%
-        filter(fips_code %in% common_counties) %>%
-        filter(date == days[i] |
-                   date == days[i+1]) %>%
-        group_by(fips_code,
-                 community_level) %>%
-        summarise(n = n()) %>%
-        arrange(fips_code)
-    consistent_low = community_level_count %>%
-        filter(community_level == "Low" &
-                   n == 2)
-    
-    consistent_medium = community_level_count %>%
-        filter(community_level == "Medium" &
-                   n == 2)
-    
-    consistent_high = community_level_count %>%
-        filter(community_level == "High" &
-                   n == 2)
-    
-    total_consistent = community_level_count %>%
-        filter(n == 2)
-    
-    no_consistent = community_level_count %>%
-        filter(n != 2)
-    
-    consistant_table = data.frame("Risk.Level.Counties" = c("Low", "Medium", "High", "Total"),
-                                  
-                                  "Consistant" = c(nrow(consistent_low),
-                                                   nrow(consistent_medium),
-                                                   nrow(consistent_high),
-                                                   nrow(total_consistent)),
-                                  
-                                  "Not.Consistant" = c(nrow(no_consistent)),
-                                  
-                                  "Consistency.Rate" = c(nrow(consistent_low)/nrow(community_level_count),
-                                                         nrow(consistent_medium)/nrow(community_level_count),
-                                                         nrow(consistent_high)/nrow(community_level_count),
-                                                         nrow(total_consistent)/nrow(community_level_count)))
-    
-    
-    fig = ggtexttable(consistant_table, rows = NULL, 
-                      theme = ttheme("mOrange"))
-    ggsave(paste("Result/twoWeeks.t", i, ".jpg", sep=""),fig, height=3,width=9,scale=1)
-    
-    
-}
-
-
-
-
-
-
-
+ggsave("Result/b2.jpg",b2, height=4,width=8,scale=1.65)
