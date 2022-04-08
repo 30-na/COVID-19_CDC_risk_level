b5d3c9900671bfe29bf481cc703b02130a7df0ac
30-na <sinamokhtar@tamu.edu>
2022-04-08 11:03:30 -0500

    do some change

--- Functions/plots_computed_consistency_01.R
+++ Functions/plots_computed_consistency_01.R
@@ -5,6 +5,7 @@ library(data.table)
 library(ggplot2)
 library(scales)
 library(ggpubr)
+library(tidyr)
 
 
 load("Data/CDC_community_level_county_computed.csv")
@@ -336,55 +337,212 @@ mix1 = grid.arrange(a2week1, a3week1, a4week1, ncol = 1, nrow = 3)
 ggsave("Result/amix1.jpg", mix1, height=8,width=8,scale=1.65)
 
 
-#########################################################
-
-
 load("Data/CDC_community_level_county_computed.csv")
-
-
-
+#########################################################
 
 
 # Computed Hospital Admission per 100k in counties with three diferent community level plot
 hos_out = boxplot(community_level_county_computed$hospital_admission_per100)$out
 
-a_hos = community_level_county_computed %>%
+hos_mu_out = community_level_county_computed %>%
     mutate(hospital_admission_per100 = replace(hospital_admission_per100,
-                                              hospital_admission_per100 %in% hos_out,0)) %>%
+                                               hospital_admission_per100 %in% hos_out,NA)) %>%
+    
+    drop_na(hospital_admission_per100)%>%
+    
+    group_by(community_level) %>%
+    summarise(mean = mean(hospital_admission_per100))
+
+hos_mu = community_level_county_computed %>%
+    group_by(community_level) %>%
+    summarise(mean = mean(hospital_admission_per100))
+
+a_hos_out = community_level_county_computed %>%
+    mutate(hospital_admission_per100 = replace(hospital_admission_per100,
+                                              hospital_admission_per100 %in% hos_out,NA)) %>%
+    drop_na(hospital_admission_per100)%>%
+    
     ggplot(aes(hospital_admission_per100, fill=community_level))+
     geom_density(alpha = .2)+
+    geom_vline(data = hos_mu_out,
+               aes(xintercept = mean,
+                   color = community_level),
+               linetype = "dashed")+
+    geom_vline(data = hos_mu,
+               aes(xintercept = mean,
+                   color = community_level),
+               linetype = "dashed")+
     facet_wrap(~community_level, nrow=3)+
-    labs(title="Computed Hospital Admission per 100k in counties")
+    labs(title="Computed Hospital Admission per 100k (outliers removed)")
 
-ggsave("Result/a_hos.jpg", a_hos, height=4,width=6,scale=1.65)
+ggsave("Result/a_hos_outlier.jpg", a_hos_out, height=4,width=6,scale=1.65)
 
-new_case = ggplot((community_level_county_computed), aes(x=community_level,
-                                                         y=new_case,
-                                                         color=community_level))+
+
+
+hos_ad = community_level_county_computed %>%
+    mutate(hospital_admission_per100 = replace(hospital_admission_per100,
+                                               hospital_admission_per100 %in% hos_out,NA)) %>%
+    drop_na(hospital_admission_per100)%>%
+    
+    ggplot(aes(x=community_level,
+                                                       y=hospital_admission_per100,
+                                                       color=community_level))+
     geom_jitter(position = position_jitter(width = 0.02))+
     geom_boxplot(alpha = 0.7, outlier.shape = NA)+
     coord_flip()+
     geom_rug()+
-    labs(title="New cases in counties with three diferent community level")
+    labs(title="Computed Hospital Admission per 100k (outliers removed) level")
 
-ggsave("Result/c_newcase.jpg", new_case, height=4,width=8,scale=1.65)
+ggsave("Result/a_hos_outlier_box.jpg", hos_ad, height=4,width=8,scale=1.65)
+
+#############################################################################
 
 
-acc_rate = ggplot((community_level_county_computed), aes(x=community_level,
-                                                         y=bed_utilization,
-                                                         color=community_level))+
+# newcase in counties with three different community level plot
+newcase_out = boxplot(community_level_county_computed$new_case)$out
+
+newcase_mu_out = community_level_county_computed %>%
+    mutate(new_case = replace(new_case, new_case %in% newcase_out, NA)) %>%
+    drop_na(new_case)%>%
+    group_by(community_level) %>%
+    summarise(mean = mean(new_case))
+
+
+new_case_mu = community_level_county_computed %>%
+    group_by(community_level) %>%
+    summarise(mean = mean(new_case))
+
+
+a_new_case_out = community_level_county_computed %>%
+    mutate(new_case = replace(new_case, new_case %in% newcase_out,NA)) %>%
+    drop_na(new_case)%>%
+    ggplot(aes(new_case, fill=community_level))+
+    geom_density(alpha = .2)+
+    geom_vline(data = newcase_mu_out,
+               aes(xintercept = mean,
+                   color = community_level),
+               linetype = "dashed")+
+    geom_vline(data = new_case_mu,
+               aes(xintercept = mean,
+                   color = community_level),
+               linetype = "dashed")+
+    facet_wrap(~community_level, nrow=3)+
+    labs(title="new case (outliers removed)")
+
+ggsave("Result/a_new_case_outlier.jpg", a_new_case_out, height=4,width=6,scale=1.65)
+
+
+
+new_case_ad = community_level_county_computed %>%
+    mutate(new_case = replace(new_case,
+                              new_case %in% newcase_out, NA)) %>%
+    drop_na(new_case)%>%
+    
+    ggplot(aes(x=community_level,
+               y=new_case,
+               color=community_level))+
     geom_jitter(position = position_jitter(width = 0.02))+
     geom_boxplot(alpha = 0.7, outlier.shape = NA)+
     coord_flip()+
     geom_rug()+
-    labs(title="Accupied Rate in counties with three diferent community level")
+    labs(title="new_case (outliers removed)")
 
-ggsave("Result/c_accupiedRate.jpg", acc_rate, height=4,width=8,scale=1.65)
+ggsave("Result/a_new_case_outlier_box.jpg", new_case_ad, height=4,width=8,scale=1.65)
+
+#############################################################################
+
+# Computed bed occupied rate in counties with three different community level plot
+bed_utilization_out = boxplot(community_level_county_computed$bed_utilization)$out
+
+
+bed_utilization_mu_out = community_level_county_computed %>%
+    mutate(bed_utilization = replace(bed_utilization,
+                                     bed_utilization %in% bed_utilization_out,NA)) %>%
+    
+    drop_na(bed_utilization)%>%
+    
+    group_by(community_level) %>%
+    summarise(mean = mean(bed_utilization))
+
+
+bed_utilization_mu = community_level_county_computed %>%
+    group_by(community_level) %>%
+    summarise(mean = mean(bed_utilization))
+
+
+a_bed_utilization_out = community_level_county_computed %>%
+    mutate(bed_utilization = replace(bed_utilization,
+                                     bed_utilization %in% bed_utilization_out,NA)) %>%
+    drop_na(bed_utilization)%>%
+    ggplot(aes(bed_utilization, fill=community_level))+
+    geom_density(alpha = .2)+
+    geom_vline(data = bed_utilization_mu_out,
+               aes(xintercept = mean,
+                   color = community_level),
+               linetype = "dashed")+
+    geom_vline(data = bed_utilization_mu,
+               aes(xintercept = mean,
+                   color = community_level),
+               linetype = "dashed")+
+    facet_wrap(~community_level, nrow=3)+
+    labs(title="Computed bed_utilization (outliers removed)")
+
+ggsave("Result/a_bed_utilization_outlier.jpg", a_bed_utilization_out, height=4,width=6,scale=1.65)
 
 
 
+bed_utilization_ad = community_level_county_computed %>%
+    mutate(bed_utilization = replace(bed_utilization,
+                                     bed_utilization %in% bed_utilization_out,NA)) %>%
+    drop_na(bed_utilization)%>%
+    
+    ggplot(aes(x=community_level,
+               y=bed_utilization,
+               color=community_level))+
+    geom_jitter(position = position_jitter(width = 0.02))+
+    geom_boxplot(alpha = 0.7, outlier.shape = NA)+
+    coord_flip()+
+    geom_rug()+
+    labs(title="Computed bed_utilization (outliers removed)")
+
+ggsave("Result/a_bed_utilization_outlier_box.jpg", bed_utilization_ad, height=4,width=8,scale=1.65)
+
+#############################################################################
 
 
+library(data.table)
 
+county = fread("Data/USA_Counties.csv")
+a = county %>% 
+    select(FIPS,
+           POPULATION) %>%
+    mutate(common = if_else(FIPS %in% common_counties, "Available Counties", "No Avalable")) %>%
+    filter(common == "Available Counties") %>%
+    mutate(sum = sum(POPULATION))
+    
+b = county %>% 
+    select(FIPS,
+           POPULATION) %>%
+    mutate(common = if_else(FIPS %in% common_counties, "Available Counties", "No Avalable")) %>%
+    filter(common == "No Avalable") %>%
+    mutate(sum = sum(POPULATION))
+
+
+ggplot(aes(x = "", y = perc, fill = answer)) +
+    geom_col(color = "black") +
+    geom_label(aes(label = labels), color = c(1, "white", "white"),
+               position = position_stack(vjust = 0.5),
+               show.legend = FALSE) +
+    guides(fill = guide_legend(title = "Answer")) +
+    scale_fill_viridis_d() +
+    coord_polar(theta = "y") + 
+    theme_void()   
+    
+   
+    
+    
+    
+    
+    
 
 
