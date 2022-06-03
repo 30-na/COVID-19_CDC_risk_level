bab37da2b66045c9010656fcbfc093298a5554db
sina mokhtar <sinamokhtar@tamu.edu>
2022-06-03 02:03:12 -0500

    add vaccine rate

--- Functions/load_vaccine_test_nursing.R
+++ Functions/load_vaccine_test_nursing.R
@@ -27,7 +27,6 @@ names(nurse_file)
 
 # clean Dataset
 
-# confirmCase = Number of residents with new laboratory positive
 # deaths = Number of residents with new suspected or laboratory positive COVID-19 who died in the facility or another location (COVID-19 DEATHS)
 # positiveTest = Number of residents with a new positive COVID-19 viral test result.
 # positiveTest_NAAT = Of the number of residents with a new positive COVID-19 viral test result, how many had positive NAAT (PCR) only
@@ -38,8 +37,7 @@ nurse_df = nurse_file %>%
            "Provider State",
            "County",
            "Passed Quality Assurance Check",
-           "Residents Weekly Confirmed COVID-19",
-           "Weekly Resident Confirmed COVID-19 Cases Per 1,000 Residents",
+           "Total Number of Occupied Beds",
            "Residents Weekly COVID-19 Deaths",
            "Weekly Resident COVID-19 Deaths Per 1,000 Residents",
            "Number of Residents with a New Positive COVID-19 Test Result",
@@ -51,8 +49,7 @@ nurse_df = nurse_file %>%
          state = "Provider State",
          county = "County",
          qualityCheck = "Passed Quality Assurance Check",
-         confirmCase = "Residents Weekly Confirmed COVID-19",
-         confirmCase_per1000 = "Weekly Resident Confirmed COVID-19 Cases Per 1,000 Residents",
+         bedOccupied = "Total Number of Occupied Beds",
          deaths = "Residents Weekly COVID-19 Deaths",
          deaths_per1000 = "Weekly Resident COVID-19 Deaths Per 1,000 Residents",
          positiveTest = "Number of Residents with a New Positive COVID-19 Test Result",
@@ -129,37 +126,68 @@ ggsave("Result/available_nursing_data_county_map.jpg", county_map, height=4,widt
 
 
 
-##### percentage of positive test rate in total test ####
-positiveTest_rate_time = nurse_df %>%
+
+# density positive rate
+# positiveTest_rate_time_na = positiveTest_rate_time %>%
+#   drop_na(positive_test_category)
+# fig_vaccine_category_rate_dens = ggplot(data = positiveTest_rate_time_na,
+#                                         aes(date,
+#                                             fill = positive_test_category)) +
+#     geom_density(position = "stack", alpha = .8) +
+#     scale_fill_brewer(palette = "Reds") +
+#     labs(title = "Positive Test Category Density")
+# 
+# ggsave("Result/fig_vaccine_category_rate_dens.jpg",
+#        fig_vaccine_category_rate_dens,
+#        height=4,width=8,scale=1.65)
+
+
+
+#####  vaccination and positive rate and death rate in county level #####
+
+nurse_categoryRate = nurse_df %>%
   mutate(totalTest = pointOfCare_test + NonePointOfCare_test) %>%
   mutate(totalTest = ifelse(totalTest < positiveTest, NA, totalTest)) %>%
-  drop_na(totalTest, positiveTest) %>%
   group_by(date, state_county) %>%
-  summarize(county_positivTest = sum(positiveTest),
+  summarize(county_vaccineRate = mean(vaccineRate),
+            county_death = sum(deaths),
+            county_bedOccupied = sum(bedOccupied),
+            county_positiveTest = sum(positiveTest),
             county_totalTest = sum(totalTest)) %>%
-  mutate(positiveTest_rate = (county_positivTest / county_totalTest) * 100) %>%
-  mutate(positive_test_category = cut(positiveTest_rate,
-                                      breaks = c(-Inf, 1, 2, 3, 4, 5, 100),
-                                      labels = c("0%-0.9%", "1%-1.9%",
-                                                 "2%-2.9%", "3%-3.9%",
-                                                 "4%-4.9%", "5%-100%")))
-##### counties positive test rate ####
-counties_positiveRate = positiveTest_rate_time %>%
+  mutate(county_deathRate = (county_death / county_bedOccupied) * 100) %>%
+  mutate(county_postiveRate = (county_positiveTest / county_totalTest) * 100) %>%
+  mutate(positiveRate_category = cut(county_postiveRate,
+                                    breaks = c(-Inf, 1, 2, 3, 4, 5, 100),
+                                    labels = c("0%-0.9%", "1%-1.9%",
+                                               "2%-2.9%", "3%-3.9%",
+                                               "4%-4.9%", "5%-100%"))) %>%
+  mutate(deathRate_category = cut(county_deathRate,
+                                     breaks = c(-Inf, .2, .4, .6, .8, 1, 100),
+                                     labels = c("0%-0.19%", "0.2%-0.39%",
+                                                "0.4%-5.9%", "0.6%-7.9%",
+                                                "0.8%-9.9%", "1%-100%"))) %>%
+  mutate(vaccineRate_category = cut(county_vaccineRate,
+                                  breaks = c(-Inf, 75, 80, 85, 90, 95, 100),
+                                  labels = c("0%-74.9%", "75%-79.9%",
+                                             "80%-84.9%", "85%-89.9%",
+                                             "90%-94.9%", "95%-100%")))
+ 
+
+##### counties positiveTest rate ####
+counties_positiveRate = nurse_categoryRate %>%
   group_by(date) %>%
   arrange(date, state_county) %>%
-  count(positive_test_category, .drop=FALSE) %>%
+  count(positiveRate_category, .drop=FALSE) %>%
   rename("count_county" = "n") %>%
-  filter(!is.na(positive_test_category)) %>%
+  filter(!is.na(positiveRate_category)) %>%
   mutate(sum_county = sum(count_county)) %>%
   mutate(counties_positive_test_rate = round(count_county / sum_county, 2))
 
-
-
-# plot the positive test category rate column
+# plot the positiveRate category rate column 
 fig_positive_test_category_rate_col = ggplot(data = counties_positiveRate,
                                              aes(x = date,
                                                  y = counties_positive_test_rate,
-                                                 fill = positive_test_category)) +
+                                                 fill = positiveRate_category)) +
   geom_col()+
   labs(title = "Proportion of counties with different positive test Rate")+
   scale_fill_manual(values=c('#ffffb2','#fed976','#feb24c','#fd8d3c','#f03b20','#bd0026'))
@@ -170,18 +198,65 @@ ggsave("Result/counties_positive_test_category_rate_nursing_col.jpg",
 
 
 
-# density positive rate
-positiveTest_rate_time_na = positiveTest_rate_time %>%
-  drop_na(positive_test_category)
-fig_vaccine_category_rate_dens = ggplot(data = positiveTest_rate_time_na,
-                                        aes(date,
-                                            fill = positive_test_category)) +
-    geom_density(position = "stack", alpha = .8) +
-    scale_fill_brewer(palette = "Reds") +
-    labs(title = "Positive Test Category Density")
+##### counties Death Rate ####
+counties_deathRate = nurse_categoryRate %>%
+  group_by(date) %>%
+  arrange(date, state_county) %>%
+  count(deathRate_category, .drop=FALSE) %>%
+  rename("count_county" = "n") %>%
+  filter(!is.na(deathRate_category)) %>%
+  mutate(sum_county = sum(count_county)) %>%
+  mutate(counties_death_rate = round(count_county / sum_county, 2))
 
-ggsave("Result/fig_vaccine_category_rate_dens.jpg",
-       fig_vaccine_category_rate_dens,
+# plot the positiveRate category rate column 
+fig_death_category_rate_col = ggplot(data = counties_deathRate,
+                                             aes(x = date,
+                                                 y = counties_death_rate,
+                                                 fill = deathRate_category)) +
+  geom_col()+
+  labs(title = "Proportion of counties with different death test Rate")+
+  scale_fill_manual(values=c('#ffffb2','#fed976','#feb24c','#fd8d3c','#f03b20','#bd0026'))
+
+ggsave("Result/counties_death_category_rate_nursing_col.jpg",
+       fig_death_category_rate_col,
        height=4,width=8,scale=1.65)
 
 
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+##### counties vaccine Rate ####
+counties_vaccineRate = nurse_categoryRate %>%
+  group_by(date) %>%
+  arrange(date, state_county) %>%
+  count(vaccineRate_category, .drop=FALSE) %>%
+  rename("count_county" = "n") %>%
+  filter(!is.na(vaccineRate_category)) %>%
+  mutate(sum_county = sum(count_county)) %>%
+  mutate(counties_vaccine_rate = round(count_county / sum_county, 2))
+
+# plot the positiveRate category rate column 
+fig_vaccine_category_rate_col = ggplot(data = counties_vaccineRate,
+                                     aes(x = date,
+                                         y = counties_vaccine_rate,
+                                         fill = vaccineRate_category)) +
+  geom_col()+
+  labs(title = "Proportion of counties with different vaccine test Rate")+
+  scale_fill_manual(values=c('#ffffb2','#fed976','#feb24c','#fd8d3c','#f03b20','#bd0026'))
+
+ggsave("Result/counties_vaccine_category_rate_nursing_col.jpg",
+       fig_vaccine_category_rate_col,
+       height=4,width=8,scale=1.65)
